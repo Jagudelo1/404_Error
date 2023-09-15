36e6d7769ee74982790e1ef0dc3fe90e976c719f
Juan David Agudelo <93266129+Jagudelo1@users.noreply.github.com>
2023-09-15 10:42:54 -0500

    404_Error V0.5

--- Js/Dark.js
+++ Js/Dark.js
@@ -1,14 +1,19 @@
 const themeButton = document.getElementById("theme-button");
+const anotherButton = document.getElementById("another-button");
 const body = document.body;
+const bulbIcon = document.querySelector(".bulb-icon");
+const otherIcon = document.querySelector(".other-icon");
 
 themeButton.addEventListener("click", () => {
     if (body.classList.contains("dark-theme")) {
         body.classList.remove("dark-theme");
         body.classList.add("light-theme");
-        themeButton.textContent = "Claro";
+        bulbIcon.innerHTML = '<i class="fa-solid fa-lightbulb fa-2xl fa-rotate-180" style="color: #fff700;"></i>';
+        otherIcon.innerHTML = '<i class="fa-solid fa-toggle-on fa-xl"></i>';
     } else {
         body.classList.remove("light-theme");
         body.classList.add("dark-theme");
-        themeButton.textContent = "Oscuro";
+        bulbIcon.innerHTML = '<i class="fa-regular fa-lightbulb fa-2xl fa-rotate-180"></i>';
+        otherIcon.innerHTML = '<i class="fa-solid fa-toggle-off fa-xl"></i>';
     }
 });
